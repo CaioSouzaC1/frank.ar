e5c985f40bc63214346e5aa37bd3a538d9419f0f
CaioSouzaC1 <caiocsouza2010@gmail.com>
2024-12-16 22:49:32 -0300

    fix: franchise middleware routes

--- api/routes/api/franchise.php
+++ api/routes/api/franchise.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\FranchiseController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(FranchiseController::class)->group(function () {
+Route::middleware(['type:admin', 'auth.api'])->controller(FranchiseController::class)->group(function () {
     Route::post('/', 'store');
     Route::delete('/{id}', action: 'destroy');
     Route::put('/{id}', action: 'update');
